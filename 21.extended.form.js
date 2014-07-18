b5afe958592c1213c0fd999701361d9eae1e5645
im4aLL <hadicse@gmail.com>
2014-07-18 21:56:18 +0600

    18.07.2014

---
+++ 21.extended.form.js
@@ -0,0 +1,53 @@
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Data', function(){
+	var db = {};
+	
+	db.countryList = [
+		{ name : 'Bangladesh', code : 'BD' },
+		{ name : 'United States', code : 'US' },
+		{ name : 'Russia', code : 'RU' }
+	];
+
+	db.user = {
+		'username' : 'John',
+		'fullname' : 'John Smith'
+	};
+
+	return db;
+});
+
+function globalCtrl($scope){
+	$scope.toggle = {};
+	$scope.toggle.state = false;
+
+	$scope.bodyClick = function(){
+		$scope.toggle.state = false;
+	};
+
+}
+
+function formCtrl($scope, Data){
+	$scope.user = Data.user;
+	$scope.countries = Data.countryList;
+
+	$scope.submitFrm = function(form){
+		console.log( $scope.user );
+		return false;
+	};
+
+	$scope.customSelect = function(object){
+		$scope.toggle.state = false;
+		$scope.user.country = object;
+	};
+
+	$scope.clickEvent = function($event){
+		$scope.toggle.state = !$scope.toggle.state;
+		$event.stopPropagation();
+	};
+
+	$scope.agreeCheck = function(){
+		$scope.user.agreed = !$scope.user.agreed;
+	}
+
+}
